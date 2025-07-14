2d0e15f3250fca364af98c3694943ac34076ea06
me-L-Codes <mel.fickinger@gmail.com>
2025-07-14 16:52:24 +0200

    Create _footer.php

---
+++ _footer.php
@@ -0,0 +1,9 @@
+
+
+
+</div> <!-- Ende .container -->
+    <footer style="text-align: center; padding: 10px; background: #333; color: white; margin-top: 20px;">
+        &copy; <?php echo date("Y"); ?> Mein Online-Tagebuch
+    </footer>
+</body>
+</html>
